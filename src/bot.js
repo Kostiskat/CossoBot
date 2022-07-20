bcdfdcd84a330393c83d78f556ad80cff98c0e49
Saltyism <80099327+Kostiskat@users.noreply.github.com>
2022-07-20 07:23:54 +0000

    added express

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,13 @@
 const { Client, Intents, Collection, MessageEmbed } = require('discord.js');
 const fs = require("fs");
+  const express = require('express');
+const server = express();
 const client = new Client({
   intents: [
-     Intents.FLAGS.GUILDS,
-     Intents.FLAGS.GUILD_MESSAGES,
-     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-     Intents.FLAGS.GUILD_MEMBERS
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MEMBERS
   ]
 });
 require("dotenv").config();
@@ -16,12 +18,31 @@ const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith
 const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
 const commandFolders = fs.readdirSync("./src/commands");
 
+// CommonJs
+
+
+// Declare a route
+
+
+// Run the server!
+
+
 (async () => {
-    for(file of functions) {
-        require(`./functions/${file}`)(client);
-    }
-    client.handleEvents(eventFiles, "./src/events");
-    client.handleCommands(commandFolders, "./src/commands");
-    client.login(process.env.token);
-    
+
+
+server.all('/', (req, res) => {
+    res.send('<h2>Server is ready!</h2>');
+});
+
+
+    server.listen(4000, () => {
+        console.log('Server Ready.');
+    });
+  for (file of functions) {
+    require(`./functions/${file}`)(client);
+  }
+  client.handleEvents(eventFiles, "./src/events");
+  client.handleCommands(commandFolders, "./src/commands");
+  client.login(process.env.token);
+
 })();
